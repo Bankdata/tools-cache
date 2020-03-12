d95f3fc5835423e9150b284e6704415103abf532
Kenneth Bøgedal <kbh@bankdata.dk>
2020-03-12 20:49:13 +0100

    :bug: fixed so the objectmapper can handle java 8+ objects

--- src/main/java/dk/bankdata/tools/factory/ObjectMapperFactory.java
+++ src/main/java/dk/bankdata/tools/factory/ObjectMapperFactory.java
@@ -3,6 +3,7 @@ package dk.bankdata.tools.factory;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 public class ObjectMapperFactory {
     private static ObjectMapper objectMapper;
@@ -14,6 +15,7 @@ public class ObjectMapperFactory {
             objectMapper = new ObjectMapper();
             objectMapper.enable(SerializationFeature.WRITE_ENUMS_USING_TO_STRING);
             objectMapper.enable(DeserializationFeature.READ_ENUMS_USING_TO_STRING);
+            objectMapper.registerModule(new JavaTimeModule());
         }
 
         return objectMapper;
